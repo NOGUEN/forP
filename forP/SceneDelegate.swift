8367417d4f7f14f100ffa07b20fb5150ceb96470
CrazyImSoFlutter <nhg1113@naver.com>
2021-04-18 11:16:34 +0900

    add collection view

--- forP/SceneDelegate.swift
+++ forP/SceneDelegate.swift
@@ -10,9 +10,9 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
-    func scene(_ Scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         let home = SearchViewController()
--- forP/SearchViewController.swift
+++ forP/SearchViewController.swift
@@ -10,22 +10,41 @@ import UIKit
 class SearchViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     var dogFoodCollectionView: UICollectionView!
     let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-    let cellWidthPadding = 10
+    let searchField: UITextField = {
+        let searchField = UITextField()
+        searchField.translatesAutoresizingMaskIntoConstraints = false
+        return searchField
+    }()
+    
+    let cellWidthPadding = 20
     let cellHeightPadding = 10
+    let foodPhoto = ["dogFood1", "dogFood2", "dogFood3", "dogFood4"]
+    let foodName = ["하이포알러제닉 가수분해 양고기", "하이포알러제닉 가수분해 소고기", "하이포알러제닉 가수분해 오리고기", "하이포알러제닉 가수분해 연어"]
+    let foodCost = ["33000", "33000", "33000", "33000"]
+    
     
     override func viewDidLoad() {
-        let viewWidth = view.frame.width
-        let viewHeight = view.frame.height
+        let view = UIView()
+        let viewWidth = self.view.frame.width
+        let viewHeight = self.view.frame.height
         let cellWidth = (Int(viewWidth) - cellWidthPadding * 3) / 2
-        let cellHeight = (Int(viewHeight) / 3)
+        let cellHeight = 200
         super.viewDidLoad()
         
         layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        layout.sectionInset = UIEdgeInsets(top: CGFloat(cellWidthPadding), left: CGFloat(cellHeightPadding), bottom: CGFloat(cellWidthPadding), right: CGFloat(cellHeightPadding))
         dogFoodCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
         
+        dogFoodCollectionView.backgroundColor = UIColor.white
         dogFoodCollectionView.delegate = self
         dogFoodCollectionView.dataSource = self
         dogFoodCollectionView.register(dogFoodCell.self, forCellWithReuseIdentifier: "cell")
+        self.view = view
+        
+        autoLayout()
+        view.addSubview(textField)
+        view.addSubview(dogFoodCollectionView)
+        
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 4
@@ -33,21 +52,103 @@ class SearchViewController: UIViewController, UICollectionViewDelegate, UICollec
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! dogFoodCell
+        cell.foodImage.image = UIImage(named: foodPhoto[indexPath.row])
+        cell.foodLabel.text = foodName[indexPath.row]
+        cell.costLabel.text = foodCost[indexPath.row]
+        cell.kgLabel.text = "(1.5kg)"
         return cell
+    }
+    
+    func autoLayout() {
+        textField.bottomAnchor.constraint(equalTo: dogFoodCollectionView.topAnchor, constant: 0).isActive = true
+        textField.widthAnchor.constraint(equalToConstant: view.frame.width - 20).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        dogFoodCollectionView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 10).isActive = true
+        
     }
 }
 
 class dogFoodCell: UICollectionViewCell {
     let button: UIButton = {
         let button = UIButton()
+        button.setImage(UIImage(named: "imageBackground"), for: .normal)
+        button.layer.cornerRadius = 12
+        button.imageView?.clipsToBounds = true
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
+    let foodImage: UIImageView = {
+        let foodImage = UIImageView()
+        foodImage.translatesAutoresizingMaskIntoConstraints = false
+        return foodImage
+    }()
+    
+    let foodLabel: UILabel = {
+        let foodLabel = UILabel()
+        foodLabel.lineBreakMode = .byCharWrapping
+        foodLabel.numberOfLines = 0
+        foodLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        foodLabel.translatesAutoresizingMaskIntoConstraints = false
+        return foodLabel
+    }()
+    
+    let costLabel: UILabel = {
+        let costLabel = UILabel()
+        costLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        costLabel.textColor = UIColor.orange
+        costLabel.translatesAutoresizingMaskIntoConstraints = false
+        return costLabel
+    }()
+    
+    let kgLabel: UILabel = {
+        let kgLabel = UILabel()
+        kgLabel.font = UIFont.systemFont(ofSize: 14)
+        kgLabel.textColor = UIColor.lightGray
+        kgLabel.translatesAutoresizingMaskIntoConstraints = false
+        return kgLabel
+    }()
+    
+    
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addViews()
+        autoLayout()
+    }
+    
+    func addViews() {
+        contentView.addSubview(button)
+        contentView.addSubview(foodImage)
+        contentView.addSubview(foodLabel)
+        contentView.addSubview(costLabel)
+        contentView.addSubview(kgLabel)
+    }
+    
+    func autoLayout() {
+        button.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0).isActive = true
+        button.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: (button.imageView?.frame.width)!).isActive = true
+        button.heightAnchor.constraint(equalToConstant: (button.imageView?.frame.height)!).isActive = true
+        button.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        button.imageView?.widthAnchor.constraint(equalToConstant: contentView.frame.width).isActive = true
         
+        foodImage.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
+        foodImage.centerYAnchor.constraint(equalTo: button.centerYAnchor).isActive = true
+        
+        foodLabel.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 0).isActive = true
+        foodLabel.leadingAnchor.constraint(equalTo: button.leadingAnchor, constant: 10).isActive = true
+        foodLabel.trailingAnchor.constraint(equalTo: button.trailingAnchor, constant: 0).isActive = true
+        foodLabel.widthAnchor.constraint(equalToConstant: button.frame.width).isActive = true
+        foodLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 30).isActive = true
+        
+        costLabel.topAnchor.constraint(equalTo: foodLabel.bottomAnchor, constant: 0).isActive = true
+        costLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
+        
+        kgLabel.centerYAnchor.constraint(equalTo: costLabel.centerYAnchor).isActive = true
+        kgLabel.leadingAnchor.constraint(equalTo: costLabel.trailingAnchor, constant: 5).isActive = true
     }
     
     required init?(coder: NSCoder) {
